51e2dea997ace6239c06fc0450e70d114d95b558
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2024-10-08 16:19:58 -0400

    Set up Next.js project and Khmer font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const notoSerifKhmer = localFont({
+  src: "./fonts/NotoSerifKhmer.ttf",
+  variable: "--font-noto-serif-khmer",
+  weight: "400",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSerifKhmer.variable} ${roboto.variable} antialiased`}
       >
         {children}
       </body>
