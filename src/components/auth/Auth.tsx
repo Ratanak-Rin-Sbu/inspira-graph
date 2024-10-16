c78642087b385a21ad22c5f57f73fcd9f80c6913
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2024-10-16 00:25:31 -0400

    Added responsive menu button for mobile

---
+++ src/components/auth/Auth.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import React, { useState } from 'react';
+import Link from "next/link";
+import styles from "./auth.module.css";
+import { Button, useMediaQuery } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import ThemeToggle from "../themeToggle/ThemeToggle";
+
+const Auth = () => {
+  const [open, setOpen] = useState(false);
+
+  // temporary
+  const status = "notauthenticated";
+
+  const isMobile = useMediaQuery('(max-width: 935px)');
+
+  return (
+    <>
+      {status === "notauthenticated" ? (
+        <div className={styles.auth}>
+          <ThemeToggle />
+          <Button
+            sx={{
+              fontFamily: 'var(--font-noto-serif-khmer), sans-serif',
+            }}
+            variant="text"
+            color="inherit"
+          >
+            លកអុីន
+          </Button>
+        </div>
+      ) : (
+        <div className={styles.auth}>
+          <ThemeToggle />
+          <Button
+            sx={{
+              fontFamily: 'var(--font-noto-serif-khmer), sans-serif',
+            }}
+            variant="text"
+            color="inherit"
+          >
+            លកអោត
+          </Button>
+        </div>
+      )}
+
+      {/* MOBILE MENU BUTTON */}
+      {isMobile && (
+        <div className={styles.mobileMenu}>
+          <ThemeToggle />
+          <MenuIcon
+            className={styles.menu}
+            onClick={() => setOpen(!open)} 
+            sx={{ fontSize: '2rem' }}
+          />
+        </div>
+      )}
+
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">សំណេរ</Link>
+          <Link href="/">សហគមន៍</Link>
+          <Link href="/">អំពី</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/">លកអុីន</Link>
+          ) : (
+            <Link href="/">លកអោត</Link>
+          )}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Auth
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,69 +1,38 @@
-import React from 'react'
-import styles from "./navbar.module.css"
-import { Button } from '@mui/material'
-import ThemeToggle from "../themeToggle/ThemeToggle";
+import React from 'react';
+import styles from "./navbar.module.css";
+import { Button } from '@mui/material';
+import Auth from '../auth/Auth';
 
 const Navbar = () => {
   return (
     <div className={`${styles.container} khmer-text text-3xl`}>
       <div className={styles.logo}>អក្ខរាវិរុទ្ធ</div>
+      
       <div className={styles.navigations}>
         <Button
-          sx={{
-            fontSize: '1.5rem',
-            fontFamily: 'var(--font-noto-serif-khmer), sans-serif',
-            lineHeight: 1.8,
-            letterSpacing: '0.05em',
-            padding: "0 5px",
-          }}
+          sx={{ fontFamily: 'var(--font-noto-serif-khmer), sans-serif' }}
           variant="text"
           color="inherit"
         >
           សំណេរ
         </Button>
         <Button
-          sx={{
-            fontSize: '1.5rem',
-            fontFamily: 'var(--font-noto-serif-khmer), sans-serif',
-            lineHeight: 1.8,
-            letterSpacing: '0.05em',
-            padding: "0 5px",
-          }}
+          sx={{ fontFamily: 'var(--font-noto-serif-khmer), sans-serif' }}
           variant="text"
           color="inherit"
         >
           សហគមន៍
         </Button>
         <Button
-          sx={{
-            fontSize: '1.5rem',
-            fontFamily: 'var(--font-noto-serif-khmer), sans-serif',
-            lineHeight: 1.8,
-            letterSpacing: '0.05em',
-            padding: "0 5px"
-          }}
+          sx={{ fontFamily: 'var(--font-noto-serif-khmer), sans-serif' }}
           variant="text"
           color="inherit"
         >
           អំពី
         </Button>
       </div>
-      <div className={styles.auth}>
-        <ThemeToggle />
-        <Button
-          sx={{
-            fontSize: '1.35rem',
-            fontFamily: 'var(--font-noto-serif-khmer), sans-serif',
-            lineHeight: 1.8,
-            letterSpacing: '0.05em',
-            // padding: "0 5px",
-          }}
-          variant="text"
-          color="inherit"
-        >
-          លកអុីន
-        </Button>
-      </div>
+
+      <Auth />
     </div>
   )
 }
