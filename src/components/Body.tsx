f95e901e75c254adf2bfac947511fef79b04a93c
Ratanak-Rin-Sbu <100118148+Ratanak-Rin-Sbu@users.noreply.github.com>
2024-10-25 00:33:45 -0400

    Hid Scroll Bar

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Intro from "@/components/intro/Intro";
+import Intro from "@/components/Intro";
 import SearchBar from "@/components/SearchBar";
+import { Box } from '@mui/material';
 
 const Body = () => {
   return (
-    <>
+    <Box minHeight="80vh">
       <Intro />
       <SearchBar />
-    </>
+    </Box>
   )
 }
 
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 
 const SearchBar: React.FC = () => {
   return (
-    <Box>
+    <Box mb="40px">
       <TextField
         variant='filled'
         sx={{
@@ -14,7 +14,7 @@ const SearchBar: React.FC = () => {
             backgroundColor: '#e6e3e3',
             height: '50px',
             fontSize: '1.25rem',
-            pb: '10px',
+            pb: '15px',
             '&:before': {
               borderBottomColor: '#d1cece',
             },
@@ -26,6 +26,11 @@ const SearchBar: React.FC = () => {
                 display: 'none',
               },
             },
+            '& .MuiInputBase-input::placeholder': {
+              fontFamily: 'var(--font-noto-serif-khmer), sans-serif',
+              lineHeight: '1.8',
+              letterSpacing: '0.07em',
+            },
           },
         }}
         InputProps={{
@@ -35,7 +40,7 @@ const SearchBar: React.FC = () => {
             </InputAdornment>
           ),
         }}
-        placeholder="Search..." // Place this outside of InputProps to avoid deprecation warning
+        placeholder="តើអ្នកកំពុងស្វែងរកសំណេរណាមួយឬ?"
       />
     </Box>
   );
